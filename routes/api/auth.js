0170716037943e10075ba31e9ea642b147027628
Kris Lafrance <kris.lafrance6067@gmail.com>
2020-03-31 22:50:09 -0700

    Adding get logged in user account route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -46,7 +46,6 @@ router.post(
 					.status(400)
 					.json({ errors: [{ msg: "Invalid Credentials" }] });
 			}
-
 			const isMatch = await bcrypt.compare(password, user.password);
 
 			if (!isMatch) {
