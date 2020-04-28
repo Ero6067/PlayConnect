7412057a206790a9c172745a04c2855a5578800e
Kris Lafrance <kris.lafrance6067@gmail.com>
2020-04-28 03:26:03 -0700

    adding get all profiles and gihub repos actions w/ reducers

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -8,6 +9,7 @@ connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("/", (req, res) => res.send("API Running"));
 
