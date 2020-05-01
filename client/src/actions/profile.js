58ce7cb93b5c37588f2afbb3b229be4fda6c16f4
Kris Lafrance <kris.lafrance6067@gmail.com>
2020-05-01 15:07:27 -0700

    fixed typo in profile.js w/ getProfileById

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -56,8 +56,9 @@ export const getProfiles = () => async (dispatch) => {
 
 // Get profile by ID
 export const getProfileById = (userId) => async (dispatch) => {
+	dispatch({ type: CLEAR_PROFILE });
 	try {
-		const res = await axios.get(`/api/profile${userId}`);
+		const res = await axios.get(`/api/profile/user/${userId}`);
 
 		dispatch({
 			type: GET_PROFILE,
--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -14,6 +14,7 @@ const Profile = ({
 		getProfileById(match.params.id);
 	}, [getProfileById]);
 
+	console.log(match.params._id);
 	return <div>PROFILE</div>;
 };
 
